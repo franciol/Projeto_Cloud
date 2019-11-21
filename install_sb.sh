b3348716a6823de9cd9f8e2cad0aebc6ea271389
franciol <fran.aveiro@hotmail.com>
2019-11-21 17:09:37 -0300

    mm

--- install_sb.sh
+++ install_sb.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 sudo apt update;
-sudo apt-get install -y apt-transport-https gnupg ca-certificates;
-echo "deb https://apache.bintray.com/couchdb-deb bionic main" | sudo tee -a /etc/apt/sources.list.d/couchdb.list;
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8756C4F765C9AC3CB6B85D62379CE192D401AB61;
-sudo apt update;
-sudo apt install -y couchdb;
+sudo snap install couchdb;
+sudo cp /Projeto_Cloud /var/snap/couchdb/current/etc -f;
