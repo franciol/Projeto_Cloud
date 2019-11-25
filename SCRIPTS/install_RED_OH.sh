29ee2e4c7983ddcb9aa5cec0a7b1338f0c5879e1
franciol <fran.aveiro@hotmail.com>
2019-11-25 13:38:00 -0300

    Adicionando arquivos e organizando

--- SCRIPTS/install_RED_OH.sh
+++ SCRIPTS/install_RED_OH.sh
@@ -1,4 +1,4 @@
 sudo apt update;
 sudo apt install python3-pip;
 pip3 install flask requests;
-nohup python3 server_RED_OH.py & > log.out;
+
--- server_red_OH.py
+++ server_red_OH.py
@@ -1,2 +1,51 @@
-import Flask
+from flask import Flask
+import requests
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/Tarefa/', methods=['GET'])
+def get_tarefas():
+    
+    return requests.get('').json()
+
+
+@app.route('/Tarefa/', methods=['POST'])
+def post_tarefas():
+    data = request.form.to_dict(flat=False)
+    if(len(dict_main) > 0):
+        id_atual = max(dict_main.keys())+1
+    else:
+        id_atual = 0
+    taref = Tarefas(id_atual, data['quando'], data['atividade'])
+    dict_main[id_atual] = taref
+    return "O id da atividade foi adicionado em %d" % (id_atual)
+
+
+@app.route('/Tarefa/<int:id>', methods=['GET'])
+def lista_especiifca(id):
+    return dict_main[id].dicttify()
+
+
+@app.route('/Tarefa/<int:id>', methods=['DELETE'])
+def apaga_especiifca(id):
+    del dict_main[id]
+    return "Item de Id %d não existe mais" % (id)
+
+
+@app.route('/Tarefa/<int:id>', methods=['PUT'])
+def altera_especiifca(id):
+    data = request.form.to_dict(flat=False)
+
+    taref = Tarefas(id, data['quando'], data['atividade'])
+    dict_main[id] = taref
+    return "A atividade de ID %d  foi alterada" % (id)
+
+
+@app.route('/healthcheck/', methods=['GET'])
+def helath():
+    return ""
 
